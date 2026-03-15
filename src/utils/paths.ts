e36838596da33058989d569435b8dcfa33f5b61f
Guilherme Camillo <guicamillo@gmail.com>
2026-03-15 11:12:17 -0700

    ensures that base urls always end with /

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,6 +2,5 @@ export const base = import.meta.env.BASE_URL;
 
 export function withBase(path: string) {
     const normalizedPath = path.startsWith("/") ? path.slice(1) : path;
-    const normalizedBase = `${base}${normalizedPath}`;
-    return normalizedBase.endsWith("/") ? normalizedBase : normalizedBase + "/";
+    return `${base}/${normalizedPath}`;
 }
